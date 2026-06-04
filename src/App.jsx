2cd3b4787a75535253901342215f4183ccb1c677
Danilo Brandão <danvabran@gmail.com>
2026-06-04 19:26:48 +0100

    Day-2-Footer design and position done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import { Routes, Route } from "react-router";
 import Homepage from "./pages/Homepage";
 import NotFoundPage from "./pages/NotFoundPage";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </div>
+
+    <div style={{ 
+      display: 'flex', 
+      flexDirection: 'column', 
+      minHeight: '100vh' 
+    }}>
+
+      <main style={{ flex: 1 }}>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </main>
+      
+      <Footer />
+    </div>  
   );
 }
 
