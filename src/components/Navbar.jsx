7926291979afcc9ff182f969e4b23ae3d2a768a1
Andy Perrotte <andy.perrotte@hotmail.com>
2026-06-04 19:52:41 +0200

    add searchbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import websiteLogo from '../assets/website-logo.png';
+import SearchBar from './SearchBar';
 
 function Navbar() {
     return (
         <nav>
             <img src={websiteLogo} alt="Website Logo" className="website-logo" />
+            <SearchBar />
         </nav>
     );
 }
