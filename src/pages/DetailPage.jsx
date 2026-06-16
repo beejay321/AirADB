879860800ae2eda1eb210a4d2ed434b1d7154e88
Oluwabusola <jibodu.busola@gmail.com>
2026-06-16 10:45:42 +0200

    resolved merge conflicts

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -40,25 +40,7 @@ function DetailPage() {
         <div className="detail-img">
           <img className="" src={currentListing.picture_url} alt="" />
         </div>
-        <div className="detail-div">
-          <div>
-            <p>{currentListing.property_type}</p>
-            <p>{currentListing.neighbourhood}</p>
-            <span>{currentListing.accommodates} guests · </span>
-            <span>{currentListing.bedrooms} bedrooms · </span>
-            <span>{currentListing.beds} beds · </span>
-            <span>{currentListing.bathroom_text}bath </span>̇
-          </div>
-          <div className="detail-host">
-            <img src={currentListing.host_picture_url} alt="" />
-            <p>Hosted by {currentListing.host_name}</p>
-            {!currentListing.host_is_superhost && <span>Superhost · </span>}
-            <span>host since {currentListing.host_since}</span>
-          </div>
-          <div>
-            <p>{currentListing.description}</p>
-          </div>
-        </div>
+
         <section className="detail-section">
           <div className="detail-div">
             <div className="detail-img-text">
